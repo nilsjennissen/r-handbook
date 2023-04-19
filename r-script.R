3b06f9623fc68d7297bbda8586c50808b28edeb9
nilsjenn <93837106+nilsjenn@users.noreply.github.com>
2023-04-19 12:51:49 +0200

    r-script.R updated

---
+++ r-script.R
@@ -0,0 +1,17 @@
+# WRITING AN R-SCRIPT
+
+# Importing the libraries
+library(ggplot2) 
+library(dplyr)
+library(tidyr)
+
+# Importing the dataset 
+dataset <- read.csv('tracklist.csv')
+
+# Perform a task
+head(dataset)
+
+# Transform some data
+
+
+# Save result to file
